8f24143f2b701237f653354298a5f9b5c3a98a10
deepthim-it19 <deepthim.it19@bitsathy.ac.in>
2019-11-11 16:08:29 +0530

    first commit

---
+++ prog67.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter a first no:");
+scanf("%d",&a);
+printf("enter an second no:");
+scanf("%d",&b);
+if(a>b)
+{
+printf("a is the greatest %d",a);
+}
+else
+{
+printf("b is the greatest %d",b);
+}
+return 0;
+}
